c0e659d607957ef7fea42839039d508b357adf33
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-09-05 20:45:54 +0000

    create server, and app controller

---
+++ controllers/AppController.js
@@ -0,0 +1,22 @@
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class AppController {
+  static async getStatus(request, response) {
+    const dbStatus = dbClient.isAlive();
+    const redisStatus = redisClient.isAlive();
+    response
+      .status(200)
+      .send({ redis: redisStatus, db: dbStatus });
+  }
+
+  static async getStats(request, response) {
+    const fileCount = await dbClient.nbFiles();
+    const userCount = await dbClient.nbUsers();
+    response
+      .status(200)
+      .send({ users: userCount, files: fileCount });
+  }
+}
+
+export default AppController;
