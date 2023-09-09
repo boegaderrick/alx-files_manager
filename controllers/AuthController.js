b417712906a32fd0d328130e9f4f605c920d1d79
Derrick Boega Nyabuto <boegaderrick@gmail.com>
2023-09-09 08:42:08 +0000

    add base64 string validation to AuthController

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -6,11 +6,23 @@ import redisClient from '../utils/redis';
 class AuthController {
   static async getConnect(request, response) {
     const authHeader = request.headers.authorization;
+    if (!authHeader.startsWith('Basic ')) {
+      response
+        .statuc(401)
+        .send({ error: 'Unauthorized' });
+      return;
+    }
     const base64String = authHeader.split(' ')[1];
     const [
       email,
       password,
     ] = Buffer.from(base64String, 'base64').toString('utf-8').split(':');
+    if (!email || !password) {
+      response
+        .status(401)
+        .send({ error: 'Unauthorized' });
+      return;
+    }
     const user = await dbClient.findOne('users', { email, password: sha1(password) });
     if (!user) {
       response
